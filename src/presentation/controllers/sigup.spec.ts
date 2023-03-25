55026d5355c6202fb0d8f865890131ea1b9ecb19
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-25 08:35:56 -0300

    refactor: move EmailValidator creation to a helper method

--- src/presentation/controllers/sigup.spec.ts
+++ src/presentation/controllers/sigup.spec.ts
@@ -7,7 +7,7 @@ interface SutTypes {
 	emailValidatorStub: EmailValidator
 }
 
-const makeSut = (): SutTypes => {
+const makeEmailValidator = (): EmailValidator => {
 	class EmailValidatorStub implements EmailValidator { //um tipo de mock que existe
 		// sempre retorne o seu mock com o valor onde não de erro
 		//onde vc quiser q falhe, você mocka e faz falhar
@@ -15,7 +15,19 @@ const makeSut = (): SutTypes => {
 			return true
 		}
 	}
-	const emailValidatorStub = new EmailValidatorStub()
+	return new EmailValidatorStub()
+}
+const makeEmailValidatorWithError = (): EmailValidator => {
+	class EmailValidatorStub implements EmailValidator {
+		isValid(email: string): boolean {
+			throw new Error();
+		}
+	}
+	return new EmailValidatorStub()
+}
+
+const makeSut = (): SutTypes => {
+	const emailValidatorStub = makeEmailValidator()
 	return {
 		sut: new SignUpController(emailValidatorStub),
 		emailValidatorStub
@@ -117,12 +129,7 @@ describe('Sign Up Controller', () => {
 	})
 
 	test('Should return 500 if EmailValidator throws', () => {
-		class EmailValidatorStub implements EmailValidator {
-			isValid(email: string): boolean {
-				throw new Error();
-			}
-		}
-		const emailValidatorStub = new EmailValidatorStub()
+		const emailValidatorStub = makeEmailValidatorWithError()
 		const sut = new SignUpController(emailValidatorStub)
 
 		const httpRequest = {
