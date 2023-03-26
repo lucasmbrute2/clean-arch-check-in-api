ab3a15585eec4732e9f703d96d8ea838b6fd1b33
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-26 20:17:43 -0300

    test: ensure DbAddAccount calls Encrypter with correct password

---
+++ src/data/use-cases/add-account/db-add-account.spec.ts
@@ -0,0 +1,23 @@
+import { DbAddAccount } from "./db-add-account"
+
+describe("DbAddAccount Use Case", () => {
+  test("Should call Encrypter with correct password", async () => {
+    class EncrypterStub {
+      async encrypt(value: string): Promise<string> {
+        return Promise.resolve('hashed_password')
+      }
+    }
+
+    const encrypterStub = new EncrypterStub()
+    const sut = new DbAddAccount(encrypterStub)
+    const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
+
+    const accountData = {
+      name: 'valid-name',
+      email: 'valid-email',
+      password: 'valid-password'
+    }
+    await sut.add(accountData)
+    expect(encryptSpy).toHaveBeenCalledWith('valid-password')
+  })
+})
