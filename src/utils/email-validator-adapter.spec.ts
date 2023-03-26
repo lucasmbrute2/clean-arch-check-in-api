8d50698092b77a9aab0e03e42920401fd69953fd
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-26 11:04:54 -0300

    test: ensure EmailValidatorAdapter returns true if validator returns true

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,12 +1,30 @@
 // Wrappers ou adapters são componentes criados para encapsular o componente do validator, estamos isolando dos controllers a validação de e-mail
-
 import { EmailValidatorAdapter } from "./email-validator-adapter"
+import validator from "validator"
+
+// O teste unitário não deve depender da validação da implementanção, a regra de negócios deve ser mockada. Nos importamos apenas com o resultado da função.
+jest.mock('validator', () => ({ // sobrescreve todos os métodos desse modulo, passando um segundo parametro sobrescrevendo o resultado mockado
+  isEmail(): boolean {
+    return true
+  },
+}))
 
 describe('EmailValidator Adapter', () => {
   test('Should return false if validator returns false', () => {
+
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+
+
     const sut = new EmailValidatorAdapter()
     const isValid = sut.isValid('invalid-email@gmail.com')
 
     expect(isValid).toBe(false)
+  })
+
+  test('Should return true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('valid-email@gmail.com')
+
+    expect(isValid).toBe(true)
   })
 })
