bc05a52afe5dc5d425808e8bc8ff225e5b882e66
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-28 21:05:18 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,30 @@
+import { MongoHelper } from "../helpers/mongo-helper"
+import { AccountMongoRepository } from "./account"
+
+describe("Account Mongodb Repository", () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+
+  test("Should return an account on success", async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'any-name',
+      email: 'any-email@gmail.com',
+      password: 'any-password'
+    })
+
+    expect(account).toBeTruthy()
+    expect(account).toHaveProperty('id')
+    expect(account).toEqual(expect.objectContaining({
+      name: 'any-name',
+      email: 'any-email@gmail.com',
+      password: 'any-password'
+    }))
+  })
+})
