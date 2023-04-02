b53d46086ede2f0f715af6a0f2bd9466db88f831
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-04-01 21:04:20 -0300

    chore: ensure that main layer will not be expected on coverage reports

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,10 @@ import { Config } from 'jest'
 const config: Config = {
   roots: ['<rootDir>/src'],
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**'
+  ],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   preset: '@shelf/jest-mongodb',
