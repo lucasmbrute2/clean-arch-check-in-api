f642a5f4394bcadad81927b1243b6e1cc22fb694
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-04-03 09:11:29 -0300

    feat: ensure LogControllerDecorator calls LogErrorRepository if controller returns a server error

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -1,10 +1,18 @@
+import { LogErrorRepository } from "../../data/protocols/log-error-repository"
 import { Controller, HttpRequest, HttpResponse } from "../../presentation/protocols"
 
 export class LogControllerDecorator implements Controller {
   // A classe que a gnt vai decorar deve ser do msm tipo da que a gnt ta implementando
-  constructor(private readonly controller: Controller) { }
+  constructor(
+    private readonly controller: Controller,
+    private readonly logErrorRepository: LogErrorRepository
+  ) { }
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    return await this.controller.handle(httpRequest)
+    const httpResponse = await this.controller.handle(httpRequest)
+    if (httpResponse.statusCode === 500) {
+      await this.logErrorRepository.log(httpResponse.body.stack)
+    }
+    return httpResponse
   }
 }
