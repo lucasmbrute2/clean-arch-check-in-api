190fca5392a8af02eab80416881ecdff2006f568
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-04-08 17:19:00 -0300

    test: ensure LoginController returns 400 if no email is provided

---
+++ src/presentation/controllers/login/login.spec.ts
@@ -0,0 +1,28 @@
+import { MissingParamError } from "../../errors"
+import { badRequest } from "../../helpers/http-helper"
+import { HttpRequest } from "../../protocols"
+import { LoginController } from "./login"
+
+interface SutTypes {
+  sut: LoginController
+}
+const makeSut = (): SutTypes => {
+  const sut = new LoginController()
+  return {
+    sut
+  }
+}
+
+describe("Login Controller", () => {
+  it("Should return 400 if no email is provided", async () => {
+    const { sut } = makeSut()
+    const httpRequest: HttpRequest = {
+      body: {
+        password: 'any-password'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
+  })
+})
