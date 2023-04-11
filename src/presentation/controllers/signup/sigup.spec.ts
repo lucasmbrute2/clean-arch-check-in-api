cecda3a29863c357dffd076fdec75d43df328cf1
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-04-10 21:02:55 -0300

    refactor: remove unused tests

--- src/presentation/controllers/signup/sigup.spec.ts
+++ src/presentation/controllers/signup/sigup.spec.ts
@@ -70,58 +70,6 @@ const makeSut = (): SutTypes => {
 }
 
 describe('Sign Up Controller', () => {
-	test('Should return 400 if no name is provided', async () => {
-		const { sut } = makeSut()
-		const httpRequest = {
-			body: {
-				email: 'johndoe@gmail.com',
-				password: '123456',
-				passwordConfirm: '123456'
-			}
-		}
-		const httpResponse = await sut.handle(httpRequest)
-		expect(httpResponse).toEqual(badRequest(new MissingParamError('name')))
-	})
-
-	test('Should return 400 if no email is provided', async () => {
-		const { sut } = makeSut()
-		const httpRequest = {
-			body: {
-				name: 'jhon doe',
-				password: '123456',
-				passwordConfirm: '123456'
-			}
-		}
-		const httpResponse = await sut.handle(httpRequest)
-		expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
-	})
-
-	test('Should return 400 if no password is provided', async () => {
-		const { sut } = makeSut()
-		const httpRequest = {
-			body: {
-				email: 'johndoe@gmail.com',
-				name: 'jhon doe',
-				passwordConfirm: '123456'
-			}
-		}
-		const httpResponse = await sut.handle(httpRequest)
-		expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
-	})
-
-	test('Should return 400 if no password confirmation is provided', async () => {
-		const { sut } = makeSut()
-		const httpRequest = {
-			body: {
-				email: 'johndoe@gmail.com',
-				name: 'jhon doe',
-				password: '123456'
-			}
-		}
-		const httpResponse = await sut.handle(httpRequest)
-		expect(httpResponse).toEqual(badRequest(new MissingParamError('passwordConfirm')))
-	})
-
 	test('Should return 400 if password confirmation fails', async () => {
 		const { sut } = makeSut()
 		const httpRequest = {
@@ -228,4 +176,4 @@ describe('Sign Up Controller', () => {
 		expect(httpResponse).toEqual(badRequest(new MissingParamError('any-field')))
 	})
 
-})   
+})
